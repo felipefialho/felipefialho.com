381e41c1f365668a1a335cd06ae7902cb5eafa51
LFeh <hi@felipefialho.com>
2019-08-06 06:03:30 -0300

    import components from folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
 
 class BlogIndex extends React.Component {
   render() {
