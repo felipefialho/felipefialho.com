925489732e235c16ca0f844df70d43d54253329a
LFeh <hi@felipefialho.com>
2019-08-07 06:06:41 -0300

    add variable file

---
+++ src/styles/variables.js
@@ -0,0 +1,54 @@
+// ==================================================
+// Variables
+//
+// 1. Spaces
+// 2. Media Queries
+// 3. Colors
+// 4. zIndex 
+// ==================================================
+
+// --------------------------------------------------
+// 1. Spaces
+// --------------------------------------------------
+
+export const Space = {
+  xxs: '.4rem',
+  xs: '.8rem',
+  sm: '1.6rem',
+  space: '2.4rem',
+  md: '3.2rem',
+  lg: '4.8rem',
+  xlg: '6.4rem',
+  xxlg: '9.6rem',
+};
+ 
+// --------------------------------------------------
+// 2. Media Queries
+// --------------------------------------------------
+
+export const Screen = {
+  xs: '360px',   // xExtra small screen / phone
+  sm: '768px',   // Extra small screen / phone
+  md: '960px',   // Small screen / tablet
+  lg: '1280px',  // Medium screen / desktop
+  xlg: '1920px',  // Extra Large screen / wide desktop
+}
+ 
+// --------------------------------------------------
+// 3. Colors
+// --------------------------------------------------
+
+export const Color = {
+  white: '#ffffff',
+  black: '#000000',
+}
+
+// --------------------------------------------------
+// 4. zIndex
+// --------------------------------------------------
+
+export const ZIndex = {
+  bg: '-1',
+  default: '1', 
+}
+ 
