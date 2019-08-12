641ba17414d80a7d1fb6b63d019d2112a94d1bf2
LFeh <hi@felipefialho.com>
2019-08-12 07:30:19 -0300

    add media-query

--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components'
+import media from 'styled-media-query';
 
 import * as V from '../../styles/variables'
 
 export const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
-  padding-left: ${V.Space.default};
-  padding-right: ${V.Space.default};
+  padding-left: ${V.Space.sm};
+  padding-right: ${V.Space.sm};
   max-width: ${V.MaxSize.lg};
+
+  ${media.greaterThan('medium')`
+    padding-left: ${V.Space.default};
+    padding-right: ${V.Space.default};
+  `}
 `
