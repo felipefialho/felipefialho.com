a8744fd1208766a261c99e153ec7bff339e9ae02
LFeh <hi@felipefialho.com>
2019-08-14 15:32:51 -0300

    add link to home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
 
 import Avatar from 'components/Avatar'
 import Menu from 'components/Menu'
@@ -13,13 +14,15 @@ const Header = ({ site: { author }}) => {
   return (
     <S.Header>
       <Container>
-        <S.Main> 
-          <S.Brand>
-            <Avatar />
-            <R.GreaterThanTablet>
-              <S.Title>{ author }</S.Title>
-            </R.GreaterThanTablet>
-          </S.Brand>
+        <S.Main>
+          <Link to="/" title="Go to home">
+            <S.Brand>
+              <Avatar />
+              <R.GreaterThanTablet>
+                <S.Title>{ author }</S.Title>
+              </R.GreaterThanTablet>
+            </S.Brand>
+            </Link>
           <S.Menu>
             <Menu />
             <R.GreaterThanTablet>
