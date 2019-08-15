67add085d2a2260538a9841b1957855ea6f22c0e
LFeh <hi@felipefialho.com>
2019-08-15 08:12:47 -0300

    improve animations

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,14 +3,21 @@ import ReactGA from 'react-ga'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 import menuLinks from './content'
+
 import * as S from './styled'
- 
+import * as V from 'styles/variables'
+
 const trackClick = item => {
   ReactGA.event({
     category: 'menu',
     action: 'click',
     label: `Menu - ${item}`
   })
+}
+
+const getActiveTheme = () => {
+  const body = document.querySelector('body');
+  return body.classList.contains('theme-dark') ? V.Color.black : V.Color.white
 }
 
 const Menu = () => {
@@ -20,7 +27,9 @@ const Menu = () => {
         <AniLink
           key={i}
           cover
-          direction="left" 
+          bg={getActiveTheme()}
+          duration={.6}
+          direction="down" 
           to={link.url}
           onClick={() => trackClick(link.label)}
           activeClassName="active">
