02c1219a43678af906db86f6878aeddf6490b5ed
LFeh <hi@felipefialho.com>
2019-08-20 08:02:07 -0300

    improve query

--- src/pages/lab.js
+++ src/pages/lab.js
@@ -5,34 +5,32 @@ import Layout from 'components/Layout'
 import SEO from 'components/Seo';
 import Lab from 'components/Lab';
 
-const { allLabJson } = useStaticQuery(
-  graphql`
-    query {
-      allLabJson {
-        edges {
-          node {
-            id,
-            path
-            title
-            date
-            description
-            imageSrc {
-              relativePath
-            }
+const labJsonQuery = graphql`
+  query {
+    allLabJson {
+      edges {
+        node {
+          id,
+          path
+          title
+          date
+          description
+          imageSrc {
+            relativePath
           }
         }
       }
     }
-  `
-)
+  }
+`
 
 const LabPage = () => {
-  const content = allLabJson.edges
-
+  const data = useStaticQuery(labJsonQuery)
+  
   return (
     <Layout>
       <SEO title="Lab" lang="en" description="Check here all my open source projects and some experiences about coding" />
-      <Lab content={content} />
+      <Lab content={data.allLabJson.edges} />
     </Layout>
   )
 }
