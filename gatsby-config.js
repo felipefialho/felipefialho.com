a68039d2d2cd21fd2e02b72875e5fa0b2621c103
LFeh <hi@felipefialho.com>
2019-08-23 18:02:29 -0300

    change image width

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 650,
             },
           },
           {
