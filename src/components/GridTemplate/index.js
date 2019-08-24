b4cafadc97daa7a52c2481868bf5af28b8ec6e81
LFeh <hi@felipefialho.com>
2019-08-24 16:28:00 -0300

    improve template

--- src/components/GridTemplate/index.js
+++ src/components/GridTemplate/index.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types'
 
 import * as S from './styled'
 
-const GridTemplate = ({ content, sidebar }) => {
+const GridTemplate = ({ content, sidebarLeft, sidebarRight }) => {
   return (
     <S.GridTemplate>
-      <aside aria-hidden={!sidebar}>{sidebar}</aside>
+      <aside aria-hidden={!sidebarLeft}>{sidebarLeft}</aside>
       <S.GridContent>{content}</S.GridContent>
+      <aside aria-hidden={!sidebarRight}>{sidebarRight}</aside>
     </S.GridTemplate>
   )
 }
