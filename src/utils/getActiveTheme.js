cb8a952c10f73a7faf8074e4af3b53a02abf17d1
LFeh <hi@felipefialho.com>
2019-08-26 11:52:00 -0300

    prevent document

--- src/utils/getActiveTheme.js
+++ src/utils/getActiveTheme.js
@@ -1,6 +1,8 @@
 import * as V from 'styles/variables'
 
 const getActiveTheme = () => {
+  if (typeof document === 'undefined') return
+
   const body = document.querySelector('body')
   return body.classList.contains('theme-dark') ? V.Color.black : V.Color.white
 }
