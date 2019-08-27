9cb219736cc5563526bad9ad28c224122a900c65
LFeh <hi@felipefialho.com>
2019-08-27 16:50:51 -0300

    add transition to box handler

--- src/components/BoxHandler/styled.js
+++ src/components/BoxHandler/styled.js
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 import * as V from 'styles/variables'
 
 export const BoxHandler = styled.article`
-  background-color: var(--bg);
-  border: ${V.Border.box};
   border-radius: ${V.BorderRadius.box};
   box-shadow: ${V.BoxShadow.box};
   color: var(--primaryColor);
@@ -12,10 +10,39 @@ export const BoxHandler = styled.article`
   height: 100%;
   min-height: 15rem;
   padding: ${V.Space.default};
-  transition: border-color ${V.Transition.default};
+  position: relative;
   will-change: border-color;
 
+  &:after,
+  &:before {
+    bottom: 0;
+    content: '';
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transition: transform ${V.Transition.default};
+  }
+
+  &:before {
+    border-left: 1px solid var(--highlightColor);
+    border-right: 1px solid var(--highlightColor);
+    transform: scaleY(0);
+  }
+
+  &:after {
+    border-bottom: 1px solid var(--highlightColor);
+    border-top: 1px solid var(--highlightColor);
+    transform: scaleX(0);
+  }
+
   &:hover {
-    border-color: var(--highlightColor);
+    &:before {
+      transform: scaleY(1)
+    }
+
+    &:after {
+      transform: scaleX(1)
+    }
   }
 `
