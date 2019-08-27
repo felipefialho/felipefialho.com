223414249c24a5f741a6a2fdded1960acdd9f847
LFeh <hi@felipefialho.com>
2019-08-27 08:11:21 -0300

    add track for ga

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,28 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactGA from 'react-ga'
 
 import { InstantSearch, SearchBox, Stats, Configure, Hits } from 'react-instantsearch-dom'
 
 import Result from './Result'
 import * as S from './styled'
 
-const Search = ({ algolia, callback }) => {
+const trackSearch = (term) => {
+  ReactGA.event({
+    category: 'Search',
+    action: 'search',
+    label: `Search - ${term}`
+  })
+}
 
+const Search = ({ algolia, callback }) => {
   return (
     <S.Search>
-      {algolia && algolia.appId && (
-        <>
+      {algolia && algolia.appId ? (
+        <div>
           <InstantSearch
             appId={algolia.appId}
             apiKey={algolia.searchOnlyApiKey}
+            onSearchStateChange={({ query }) => trackSearch(query)}
             indexName={algolia.indexName}>
             <Configure hitsPerPage={100} distinct />
             <SearchBox
               autoFocus
               translations={{ placeholder: 'Pesquisar no blog...' }} />
-            <Stats translations={{ stats(nbHits, timeSpentMS) {
-              return `${nbHits} resultados encontrados`
-            }}} />
+            <Stats translations={{
+              stats(nbHits, timeSpentMS) {
+                return `${nbHits} resultados encontrados`
+              }
+            }} />
             <S.SearchResults>
               <Hits hitComponent={Result} />
               {callback}
@@ -32,8 +43,10 @@ const Search = ({ algolia, callback }) => {
             Powered by Algolia
             <S.AlgoliaIcon />
           </S.Title>
-        </>
-      )}
+        </div>
+      ) : (
+        <nav>{callback}</nav>
+      )}}
     </S.Search>
   )
 }
