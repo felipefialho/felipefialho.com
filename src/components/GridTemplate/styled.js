ef48963efaea731071106ec4c17d1960c5550e0e
LFeh <hi@felipefialho.com>
2019-08-27 10:08:21 -0300

    improve layout

--- src/components/GridTemplate/styled.js
+++ src/components/GridTemplate/styled.js
@@ -4,7 +4,7 @@ import media from 'styled-media-query'
 import * as V from 'styles/variables'
 
 export const GridTemplate = styled.div`
-  ${media.greaterThan('medium')`
+  ${media.greaterThan('large')`
     display: grid;
     grid-column-gap: ${V.Space.default};
     grid-template-columns: minmax(10rem, 15rem) minmax(65rem, 1fr) minmax(10rem, 15rem);
