72c8b83ed5149225f6a860890c66378bee1ec45a
LFeh <hi@felipefialho.com>
2019-08-27 10:09:23 -0300

    add index name

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -24,7 +24,7 @@ const content = ({ list }) => {
   const algolia = {
     appId: process.env.GATSBY_ALGOLIA_APP_ID,
     searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-    indexName: process.env.ALGOLIA_ADMIN_KEY
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME
   }
 
   return (
