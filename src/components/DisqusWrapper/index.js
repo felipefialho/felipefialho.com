33d5d90694c3125a80d7364ac37ad175e8911408
LFeh <hi@felipefialho.com>
2019-08-30 10:04:20 -0300

    improve disqus

--- src/components/DisqusWrapper/index.js
+++ src/components/DisqusWrapper/index.js
@@ -5,7 +5,7 @@ import ReactDisqusComments from 'react-disqus-comments'
 import * as S from './styled'
 
 const DisqusWrapper = ({ slug, title }) => {
-  const url = `https://felipefialho.com${slug}`
+  const url = `https://felipefialho.com/blog${slug}`
   return (
     <S.DisqusWrapper>
       <ReactDisqusComments
