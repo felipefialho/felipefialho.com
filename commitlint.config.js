1b71b31c16aaa0d4ba899358aecfc4e76c846c13
felipefialho <hi@felipefialho.com>
2020-04-21 12:57:41 -0300

    chore: add commitlint and husky

---
+++ commitlint.config.js
@@ -0,0 +1,3 @@
+// See more here
+// https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional
+module.exports = { extends: ['@commitlint/config-conventional'] }
