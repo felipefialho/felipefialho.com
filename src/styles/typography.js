9766933cd68040f5edd158959fecaa7cca56a536
felipefialho <hi@felipefialho.com>
2020-06-06 16:27:33 -0300

    feat: add styled components

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,98 +1,117 @@
-// ==================================================
-// Typography
-// ==================================================
+import { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import * as V from 'styles/variables'
 
-export const Title = `
-  color: var(--primaryColor);
-  font-size: 3.4rem;
-  line-height: 1.1em;
+export const Title = css`
+  & {
+    color: var(--primaryColor);
+    font-size: 3.4rem;
+    line-height: 1.1em;
 
-  @media (min-width: 768px) {
-    font-size: 4.2rem;
+    ${media.greaterThan('medium')`
+      font-size: 4.2rem;
+    `}
   }
 `
 
-export const Subtitle1 = `
-  color: var(--secondaryColor);
-  font-size: 2.1rem;
-  letter-spacing: -0.45px;
-  line-height: 1.5em;
+export const Subtitle1 = css`
+  & {
+    color: var(--secondaryColor);
+    font-size: 2.1rem;
+    letter-spacing: -0.45px;
+    line-height: 1.5em;
 
-  @media (min-width: 768px) {
-    font-size: 2.4rem;
+    ${media.greaterThan('medium')`
+      font-size: 2.4rem;
+    `}
   }
 `
 
-export const Subtitle2 = `
+export const Subtitle2 = css`
+ & {
   color: var(--secondaryColor);
   font-size: 2rem;
   letter-spacing: -0.04px;
   line-height: 1.3em;
+ }
 `
 
-export const Heading1 = `
-  color: var(--primaryColor);
-  font-size: 2.6rem;
-  font-weight: 600;
-  letter-spacing: -0.022em;
-  line-height: 1.3em;
+export const Heading1 = css`
+  & {
+    color: var(--primaryColor);
+    font-size: 2.6rem;
+    font-weight: 600;
+    letter-spacing: -0.022em;
+    line-height: 1.3em;
 
-  @media (min-width: 768px) {
-    font-size: 3rem;
+    ${media.greaterThan('medium')`
+      font-size: 3rem;
+    `}
   }
 `
 
-export const Heading2 = `
-  color: var(--primaryColor);
-  font-size: 2.1rem;
-  font-weight: 600;
-  letter-spacing: -0.04px;
-  line-height: 1.5em;
+export const Heading2 = css`
+  & {
+    color: var(--primaryColor);
+    font-size: 2.1rem;
+    font-weight: 600;
+    letter-spacing: -0.04px;
+    line-height: 1.5em;
 
-  @media (min-width: 768px) {
-    font-size: 2.4rem;
+    ${media.greaterThan('medium')`
+      font-size: 2.4rem;
+    `}
   }
 `
 
-export const Heading3 = `
-  color: var(--primaryColor);
-  font-size: 2.1rem;
-  font-weight: 400;
-  letter-spacing: -0.04px;
-  line-height: 1.5em;
+export const Heading3 = css`
+  & {
+    color: var(--primaryColor);
+    font-size: 2.1rem;
+    font-weight: 400;
+    letter-spacing: -0.04px;
+    line-height: 1.5em;
 
-  @media (min-width: 768px) {
-    font-size: 2.4rem;
+    ${media.greaterThan('medium')`
+      font-size: 2.4rem;
+    `}
   }
 `
 
-export const Heading4 = `
-  color: var(--primaryColor);
-  font-size: 2rem;
-  font-weight: 600;
-  letter-spacing: -0.04px;
-  line-height: 1.5em;
+export const Heading4 = css`
+  & {
+    color: var(--primaryColor);
+    font-size: 2rem;
+    font-weight: 600;
+    letter-spacing: -0.04px;
+    line-height: 1.5em;
+  }
 `
 
-export const Text1 = `
-  font-size: 2rem;
-  font-weight: 400;
-  letter-spacing: -0.04px;
-  line-height: 1.5em;
+export const Text1 = css`
+  & {
+    font-size: 2rem;
+    font-weight: 400;
+    letter-spacing: -0.04px;
+    line-height: 1.5em;
+  }
 `
 
-export const Text2 = `
-  font-size: 1.6rem;
-  font-weight: 400;
-  letter-spacing: -0.14px;
-  line-height: 1.55em;
+export const Text2 = css`
+  & {
+    font-size: 1.6rem;
+    font-weight: 400;
+    letter-spacing: -0.14px;
+    line-height: 1.55em;
+  }
 `
 
-export const LinkTransition = `
-  position: relative;
-  will-change: transform;
+export const LinkTransition = css`
+  & {
+    position: relative;
+    will-change: transform;
+  }
 
   &:after {
     background-color: var(--highlightColor);
@@ -114,7 +133,9 @@ export const LinkTransition = `
   }
 `
 
-export const Link = `
-  color: var(--highlightColor);
-  ${LinkTransition}
+export const Link = css`
+  & {
+    color: var(--highlightColor);
+    ${LinkTransition}
+  }
 `
