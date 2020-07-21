1f53c24d10fae5ee468b4fc45d26d13554ef2600
felipefialho <hi@felipefialho.com>
2020-07-21 20:04:21 -0300

    chore: change date to 2020

--- src/components/FooterData/index.js
+++ src/components/FooterData/index.js
@@ -8,7 +8,7 @@ const FooterData = () => {
   return (
     <S.FooterData>
       <Container>
-        ©2019 felipefialho.com
+        ©2020 felipefialho.com
         <a className="link" href="/policies">Política de Privacidade</a>
         <a className="link" href="https://github.com/felipefialho/felipefialho.com/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">Termos de Uso</a>
       </Container>
