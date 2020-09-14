b6977b17f0f64521ed565ba633168aad4afebe1f
felipefialho <hi@felipefialho.com>
2020-09-14 12:17:35 -0300

    feat(postfooter): add pay component

--- src/components/PostFooter/index.js
+++ src/components/PostFooter/index.js
@@ -1,37 +1,66 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 
 import * as S from './styled'
 
+const trackClickOnPay = (item) => {
+  ReactGA.event({
+    category: 'Pay',
+    action: 'click',
+    label: `Pay - ${item}`
+  })
+}
+
 const PostFooter = () => {
   return (
-    <S.PostFooter>
-      <S.PostFooterContent>
-        <S.Title>
-          Encontrou algum problema no texto?
+    <>
+      <S.PostFooter>
+        <S.PostFooterContent>
+          <S.Title>
+            Gostou do conteúdo? Aceito um café <span
+              className="small"
+              role="img"
+              aria-label="Emoji de café">☕️</span>
+            <span
+              className="small"
+              role="img"
+              aria-label="Emoji com olhos de coração">😍</span>
+          </S.Title>
+          <S.PostFooterPay>
+            <a href="https://app.picpay.com/user/felipe.fialho10" target="_blank" rel="noopener noreferrer" title="Transferir no PicPay" onClick={() => trackClickOnPay('PicPay')}>PicPay (qualquer valor)</a>
+            <a href="https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=93725796-11d360a5-36b9-4f63-bca6-f605e5e3931a" target="_blank" rel="noopener noreferrer" title="Transferir no MercadoPago" onClick={() => trackClickOnPay('MercadoPago')}>MercadoPago (R$ 5)</a>
+          </S.PostFooterPay>
+        </S.PostFooterContent>
+      </S.PostFooter>
+      <S.PostFooter>
+        <S.PostFooterContent>
+          <S.Title>
+            Encontrou algum problema no texto?
           Me ajude a corrigir! <span
+              className="small"
+              role="img"
+              aria-label="Emoji sorrindo">😁</span>
+          </S.Title>
+
+          <p>Esse projeto é open source, então basta alterar o arquivo de texto, <a href="https://github.com/felipefialho/felipefialho.com/tree/master/content/posts" target="_blank" rel="noopener noreferrer" title="Alterar texto">diretamente no Github</a> e abrir um pull-request. É uma maneira simples e eficaz de contribuir com a comunidade de desenvolvimento web.</p>
+        </S.PostFooterContent>
+
+        <S.PostFooterContent>
+          <S.Title>Me acompanhe por ai! <span
             className="small"
             role="img"
-            aria-label="Emoji sorrindo">😁</span>
-        </S.Title>
+            aria-label="Emoji mostrando a lingua">😜</span>
+          </S.Title>
 
-        <p>Esse projeto é open source, então basta alterar o arquivo de texto, <a href="https://github.com/felipefialho/felipefialho.com/tree/master/content/posts" target="_blank" rel="noopener noreferrer" title="Alterar texto">diretamente no Github</a> e abrir um pull-request. É uma maneira simples e eficaz de contribuir com a comunidade de desenvolvimento web.</p>
-      </S.PostFooterContent>
-
-      <S.PostFooterContent>
-        <S.Title>Me acompanhe por ai! <span
-          className="small"
-          role="img"
-          aria-label="Emoji mostrando a lingua">😜</span>
-        </S.Title>
-
-        <p>
-          - Youtube: <a href="https://youtube.com/FelipeFialhoDev" target="_blank" rel="noopener noreferrer" title="Ver Youtube">@FelipeFialhoDev</a> <br />
+          <p>
+            - Youtube: <a href="https://youtube.com/FelipeFialhoDev" target="_blank" rel="noopener noreferrer" title="Ver Youtube">@FelipeFialhoDev</a> <br />
           - Twitter: <a href="https://twitter.com/felipefialho_" target="_blank" rel="noopener noreferrer" title="Ver Twitter">@felipefialho_</a> <br />
           - Github: <a href="https://github.com/felipefialho" target="_blank" rel="noopener noreferrer" title="Ver Github">@felipefialho</a> <br />
           - LinkedIn: <a href="https://www.linkedin.com/in/felipefialho/" target="_blank" rel="noopener noreferrer" title="Ver Linkedin">@felipefialho</a>
-        </p>
-      </S.PostFooterContent>
-    </S.PostFooter>
+          </p>
+        </S.PostFooterContent>
+      </S.PostFooter>
+    </>
   )
 }
 
--- src/components/PostFooter/styled.js
+++ src/components/PostFooter/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 import * as V from 'styles/variables'
 import * as T from 'styles/typography'
@@ -15,7 +16,7 @@ export const PostFooterContent = styled.div`
   position: relative;
 
   &:after {
-    border-top: ${V.Border.default}; 
+    border-top: ${V.Border.default};
     content: '';
     left: 20%;
     position: absolute;
@@ -29,5 +30,38 @@ export const Title = styled.h2`
 
   &:not(:last-child) {
     margin-bottom: ${V.Space.sm};
+  }
+`
+
+export const PostFooterPay = styled.section`
+  ${media.greaterThan('medium')`
+    display: flex;
+    justify-content: space-between;
+  `}
+
+  > a {
+    border: ${V.Border.default};
+    color: var(--primaryColor);
+    display: block;
+    padding: ${V.Space.sm};
+    text-align: center;
+    transition: border-color ${V.Transition.default};
+    will-change: border-color;
+
+    ${media.lessThan('small')`
+      &:not(:last-child) {
+        margin-bottom: ${V.Space.xs};
+      }
+    `}
+
+    ${media.greaterThan('medium')`
+      flex: 1;
+      margin: 0 ${V.Space.sm};
+    `}
+
+    &:hover,
+    &:focus {
+      border-color: var(--highlightColor);
+    }
   }
 `
